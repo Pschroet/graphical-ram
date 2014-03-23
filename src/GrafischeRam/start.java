68e1826b266c6bbc8ddb2decbdcad328dca80a50
Pschroet <pschroet@inf.fu-berlin.de>
2014-03-23 22:16:39 +0100

    changed Graphic to GraphicSwing to modularize graphic

--- src/GrafischeRam/start.java
+++ src/GrafischeRam/start.java
@@ -5,6 +5,6 @@ public class start{
 	public static void main(String[] args){
 		Ram ram = new Ram();
 		@SuppressWarnings("unused")
-		Graphic GUI = new Graphic(ram);
+		GraphicSwing GUI = new GraphicSwing(ram);
 	}
 }
