4eef45f7501305f208278c2618f6b66c92ae5bbe
Pschroet <pschroet@inf.fu-berlin.de>
2014-04-21 21:09:29 +0200

    made git ignore .gitignore

--- src/GrafischeRam/start.java
+++ src/GrafischeRam/start.java
@@ -3,8 +3,6 @@ package GrafischeRam;
 public class start{
 	
 	public static void main(String[] args){
-		Ram ram = new Ram();
-		@SuppressWarnings("unused")
-		GraphicSwing GUI = new GraphicSwing(ram);
+		new GraphicSwing(new Ram());
 	}
 }
